7d0bc871d2366276c46f0ea697db55bfbf18ffa5
FranciscoG0 <tefelfrancisco@gmail.com>
2024-06-05 09:34:44 -0600

    Prueba01

---
+++ TraductorMaya/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Esto es solo un ejemplo de cómo podrías añadir interactividad en el futuro.
+    console.log("Página cargada y lista para interactuar.");
+});
